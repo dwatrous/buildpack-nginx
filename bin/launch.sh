b7f2b5339e5071f15465c2025a355b157c3ac2a6
Daniel Watrous <daniel@danielwatrous.com>
2014-10-14 09:11:36 -0500

    add some debug information

--- bin/launch.sh
+++ bin/launch.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+pwd
+ls -la $HOME
+
 mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
 sed "s/\(^\s*listen\s*\)80/\1 $PORT/" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
+cat $HOME/nginx/conf/nginx.conf
 
-$HOME/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
+
+$HOME/nginx/bin/nginx -c $HOME/nginx/conf/nginx.conf -p $HOME/nginx
