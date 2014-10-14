33b87ed5d9a782eea064673e01d78eafd82ee40c
Daniel Watrous <dwmaillist@gmail.com>
2014-10-14 10:48:13 -0500

    update web root

--- bin/launch.sh
+++ bin/launch.sh
@@ -5,6 +5,7 @@ ls -la $HOME
 
 mv $HOME/nginx/conf/nginx.conf $HOME/nginx/conf/nginx.conf.original
 sed "s/\(^\s*listen\s*\)80/\1 $PORT/" $HOME/nginx/conf/nginx.conf.original > $HOME/nginx/conf/nginx.conf
+sed -i '' "s/\(^\s*root\s*\)html/\1 $HOME\/public/" $HOME/nginx/conf/nginx.conf
 cat $HOME/nginx/conf/nginx.conf
 
 
